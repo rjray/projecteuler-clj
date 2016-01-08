82ab60f8427f3094440d265916089db83a39d2d3
Randy J. Ray <rjray@blackperl.com>
2016-01-08 15:45:31 -0800

    Added code for problem 15. Also have submitted 16.

--- src/projecteuler/core.clj
+++ src/projecteuler/core.clj
@@ -253,6 +253,13 @@
                   (map #(list % (collatz-chain-recursive %))
                        (range 1 maximum))))))
 
+;; https://projecteuler.net/problem=15
+;; See also https://en.wikipedia.org/wiki/Binomial_coefficient
+(defn lattice-paths [& [k]]
+  (let [k (or k 20)
+        n (* k 2)]
+    (apply * (map #(/ (- (+ n 1) %) %) (range 1 (inc k))))))
+
 ;; https://projecteuler.net/problem=16
 (defn power-digit-sum [& [x n]]
   (let [x (or x 2)
