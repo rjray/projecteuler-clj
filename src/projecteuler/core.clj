ed5f8fc7097e159de060ff29061236956ecceda1
Randy J. Ray <rjray@blackperl.com>
2016-01-16 00:01:05 -0800

    Faster soln for #99.

--- src/projecteuler/core.clj
+++ src/projecteuler/core.clj
@@ -379,17 +379,28 @@
 ;; https://projecteuler.net/problem=99
 (defn- pair-to-bigint [base exp]
   (.pow (BigInteger. base) (Integer/parseInt exp)))
-(defn largest-exponential [& [source]]
+(defn largest-exponential-naive [& [source]]
   (let [source (or source "data/099.txt")
-        data   (slurp source)]
+        data   (slurp source)
+        seq    (re-seq #"(\d+),(\d+)" data)]
     (last (last (sort #(compare (first %1) (first %2))
                       (map-indexed #(list (apply pair-to-bigint (rest %2))
                                           (inc %1))
-                                   (re-seq #"(\d+),(\d+)" data)))))))
-(defn largest-exponential-parallel [& [source]]
+                                   seq))))))
+(defn largest-exponential-naive-parallel [& [source]]
   (let [source (or source "data/099.txt")
         data   (slurp source)
         seq    (re-seq #"(\d+),(\d+)" data)]
     (last (last (sort #(compare (first %1) (first %2))
                       (pmap #(list (apply pair-to-bigint (rest %)) (first %))
                             (map-indexed #(cons (inc %1) (rest %2)) seq)))))))
+(defn- pair-to-expnum [base exp]
+  (* (Integer/parseInt exp) (Math/log10 (Integer/parseInt base))))
+(defn largest-exponential [& [source]]
+  (let [source (or source "data/099.txt")
+        data   (slurp source)
+        seq    (re-seq #"(\d+),(\d+)" data)]
+    (last (last (sort #(compare (first %1) (first %2))
+                      (map-indexed #(list (apply pair-to-expnum (rest %2))
+                                          (inc %1))
+                                   seq))))))
