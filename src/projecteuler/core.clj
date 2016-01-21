26961283d38458535908648ceee2c209077e40a8
Randy J. Ray <rjray@blackperl.com>
2016-01-21 11:00:15 -0800

    Solution for #27.

--- src/projecteuler/core.clj
+++ src/projecteuler/core.clj
@@ -346,6 +346,16 @@
 ;; https://projecteuler.net/problem=26
 
 ;; https://projecteuler.net/problem=27
+(defn- is-prime? [n]
+  (= n (first (drop-while #(< % n) primes))))
+(defn- quadratic [n a b]
+  (+ (* n n) (* a n) b))
+(defn- prime-list [a b]
+  (take-while is-prime? (map #(quadratic % a b) (iterate inc 0))))
+(defn quadratic-primes-prod []
+  (second (first (sort #(compare (first %2) (first %1))
+                       (for [a (range -999 1000), b (range -999 1000)]
+                         (list (count (prime-list a b)) (* a b)))))))
 
 ;; https://projecteuler.net/problem=28
 (def corners-seq
