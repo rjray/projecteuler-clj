55f0f5d96f45aad681e9799e821705649f977e3f
Randy J. Ray <rjray@blackperl.com>
2016-01-22 16:43:59 -0800

    Solution for #41.

---
+++ src/projecteuler/p041.clj
@@ -0,0 +1,15 @@
+(ns projecteuler.core
+  (:require [projecteuler.core :refer [prime?]]
+            [clojure.math.combinatorics :as comb]))
+
+;; https://projecteuler.net/problem=41
+
+(defn- digit-permutations [from to]
+  (apply concat (map comb/permutations (map #(range 1 (inc %))
+                                            (range from (inc to))))))
+(defn- permutation-to-num [p]
+  (Integer/parseInt (reduce str p)))
+
+(defn largest-pandigital-prime []
+  (first (filter prime? (map permutation-to-num
+                             (reverse (digit-permutations 4 9))))))
