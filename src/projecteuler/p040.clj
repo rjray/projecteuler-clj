6dfe0ea37682822794248be32c2a80303ca17e40
Randy J. Ray <rjray@blackperl.com>
2016-02-03 16:24:18 -0800

    Solution for #40.

---
+++ src/projecteuler/p040.clj
@@ -0,0 +1,15 @@
+(ns projecteuler.core
+  (:require [projecteuler.core :refer [num-map]]))
+
+;; https://projecteuler.net/problem=40
+
+(defn- pow-10 [n]
+  (reduce * (repeat n 10)))
+(defn- get-digit [n]
+  (loop [n n, s 1]
+    (cond
+     (> n (count (str s))) (recur (- n (count (str s))) (inc s))
+     :else (num-map (nth (str s) (dec n))))))
+
+(defn champernowne []
+  (reduce * (map #(get-digit (pow-10 %)) (range 7))))
