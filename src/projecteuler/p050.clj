6dd68354a9f5bec8cce959c09abf25494f6ba70f
Randy J. Ray <rjray@blackperl.com>
2016-02-12 14:13:26 -0800

    Solution for #50.

---
+++ src/projecteuler/p050.clj
@@ -0,0 +1,20 @@
+(ns projecteuler.core
+  (:require [projecteuler.core :refer [primes is-prime?]]))
+
+;; https://projecteuler.net/problem=50
+
+(def ^:private prime-sums
+  (map first
+       (iterate (fn [[sum s]] [(+ sum (first s)) (rest s)])
+                [0 primes])))
+
+(defn consecutive-prime-sums [& [maxval]]
+  (let [maxval (or maxval 1000000)]
+    (loop [c 1]
+      (let [sums (reverse (take c prime-sums))
+            subs (take c (reverse (take-while #(< (- % (last sums)) maxval)
+                                              (rest prime-sums))))]
+        (if-let [el (some #(if (prime? %) % nil)
+                          (map #(- %1 %2) subs sums))]
+          el
+          (recur (inc c)))))))
