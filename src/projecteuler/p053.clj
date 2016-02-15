9826a3da1da6d44aae18f42a188ddf1f1fccc65d
Randy J. Ray <rjray@blackperl.com>
2016-02-15 08:54:29 -0800

    Solution for #53.

---
+++ src/projecteuler/p053.clj
@@ -0,0 +1,13 @@
+(ns projecteuler.core
+  (:require [projecteuler.core :refer [!]]))
+
+;; https://projecteuler.net/problem=53
+
+(defn- nCr [n r]
+  (/ (! n) (*' (! r) (! (- n r)))))
+
+(defn combinatoric-selections [& [maxn]]
+  (let [maxn (or maxn 100)]
+    (->> (for [n (range 23 (inc maxn)), r (range 1 (inc n))] (nCr n r))
+         (filter #(> % 1000000))
+         (count))))
