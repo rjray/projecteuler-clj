ad0ecdd6f90aa3c805740a43074b45f9b86d134d
Randy J. Ray <rjray@blackperl.com>
2016-02-16 11:28:47 -0800

    Solution for #56.

---
+++ src/projecteuler/p056.clj
@@ -0,0 +1,16 @@
+(ns projecteuler.core
+  (:require [clojure.math.numeric-tower :as math]
+            [projecteuler.core :refer [num-map]]))
+
+;; https://projecteuler.net/problem=56
+
+(defn- digit-sum [n]
+  (reduce + (map num-map (str n))))
+
+(defn powerful-digit-sum [& [maxval]]
+  (let [maxval (or maxval 100)]
+    (loop [a 2, b 1, cur 0]
+      (cond
+       (= a maxval) cur
+       (= b maxval) (recur (inc a) 1 cur)
+       :else        (recur a (inc b) (max cur (digit-sum (math/expt a b))))))))
