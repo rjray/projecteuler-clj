7aa8d1272b5e0aedf29ea70affafbe6e26db2d62
Randy J. Ray <rjray@blackperl.com>
2016-02-19 23:09:17 -0800

    Solution for #98. Known to be too slow.

---
+++ src/projecteuler/p098.clj
@@ -0,0 +1,66 @@
+(ns projecteuler.core
+  (:require [clojure.math.combinatorics :as comb]))
+
+;; https://projecteuler.net/problem=98
+
+(def ^:private all-permutations
+  (vec (map #(apply concat (map comb/permutations
+                                (comb/combinations (range 10) %))) (range 10))))
+(def ^:private all-squares
+  (set (map str (map #(* % %) (range 1 (inc (Math/sqrt 999999999)))))))
+
+(def ^:private not-nil? (complement nil?))
+
+(defn- word-key [s]
+  (apply str (sort (seq s))))
+
+(defn- word-key-distinct [s]
+  (apply str (distinct (sort (seq s)))))
+
+(defn- encode-words [key perm ws]
+  (let [keymap (zipmap (seq key) perm)]
+    (map #(apply str (map keymap %)) ws)))
+
+(defn- square-pair? [s]
+  (if (and (all-squares (first s)) (all-squares (last s))) s))
+
+(defn- get-words [file]
+  (re-seq #"\w+" (slurp file)))
+
+(defn- get-anagrams [words]
+  (filter #(> (count %) 1) (vals (group-by word-key words))))
+
+(defn- anagram-pairs [anagrams]
+  (apply concat (map #(comb/combinations % 2) anagrams)))
+
+(defn- keyed-anagram-pairs [pairs]
+  (group-by #(word-key-distinct (first %)) pairs))
+
+(defn- test-one-pair [k pair]
+  (let [perms (all-permutations (count k))]
+    (filter not-nil? (map #(square-pair? (encode-words k % pair)) perms))))
+
+(defn- process-pairs [k pairs]
+  (filter not-nil? (map #(test-one-pair k %) pairs)))
+
+(defn- process-keys [key-seq key-pairs-map]
+  (filter not-nil? (map #(process-pairs % (key-pairs-map %)) (sort key-seq))))
+
+(defn- find-max-square-from-pairs [keyed-pairs]
+  (let [pair-keys   (keys keyed-pairs)
+        keys-by-len (group-by count pair-keys)]
+    (loop [n 9, acc ()]
+      (let [s (flatten (process-keys (keys-by-len n) keyed-pairs))]
+        (cond
+         (= n 3)    (last (sort (map #(Integer/parseInt %) acc)))
+         (empty? s) (recur (dec n) acc)
+         :else      (recur (dec n) (concat acc s)))))))
+
+(defn anagramic-squares [& [file]]
+  (let [file (or file "data/098.txt")]
+    (->> file
+         (get-words)
+         (get-anagrams)
+         (anagram-pairs)
+         (keyed-anagram-pairs)
+         (find-max-square-from-pairs))))
