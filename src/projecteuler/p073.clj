76a6e49c91e45d6e3979b25911ccf7f86c0678dd
Randy J. Ray <rjray@blackperl.com>
2016-02-23 16:51:06 -0800

    Solution for #73.

---
+++ src/projecteuler/p073.clj
@@ -0,0 +1,13 @@
+(ns projecteuler.core
+  (:require [clojure.math.numeric-tower :as math]))
+
+;; https://projecteuler.net/problem=73
+
+(defn- count-in-range [d]
+  (let [top (int (math/ceil (/ d 2)))
+        bot (int (math/floor (/ d 3)))]
+    (count (filter #(= 1 (math/gcd % d)) (range (inc bot) top)))))
+
+(defn counting-fractions-in-range [& [maxd]]
+  (let [maxd (or maxd 12000)]
+    (reduce + (pmap count-in-range (range 5 (inc maxd))))))
