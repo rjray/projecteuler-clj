16b3b51074b547984d6cec5ee391a9ccdc981f67
Randy J. Ray <rjray@blackperl.com>
2016-02-24 16:50:26 -0800

    Solution for #79.

---
+++ src/projecteuler/p079.clj
@@ -0,0 +1,48 @@
+(ns projecteuler.core
+  (:require [clojure.set :as sets]))
+
+;; https://projecteuler.net/problem=79
+
+(defn- graph-one-try [graph try]
+  (loop [graph    graph
+         [d & ds] (seq try)]
+    (cond
+     (nil? d) graph
+     :else    (recur (assoc graph d (sets/union (graph d #{}) (set ds))) ds))))
+
+(defn- graph-of-tries [all-tries]
+  (loop [graph         {}
+         [try & tries] all-tries]
+    (cond
+     (nil? try) graph
+     :else      (recur (graph-one-try graph try) tries))))
+
+(defn- enqueue-neighbors [queue neighbors path]
+  (concat queue
+          (apply concat (map #(list (list % (cons % path))) neighbors))))
+
+(defn- find-solution [start graph universe target-len]
+  (loop [queue (list (list start (list start)))
+         solns ()]
+    (cond
+     (empty? queue) solns
+     :else
+     (let [[head & qs] queue
+           [curr path] head
+           neighbors   (graph curr #{})]
+       (if (= target-len (count path))
+         (recur qs (cons (apply str (reverse path)) solns))
+         (recur (enqueue-neighbors qs neighbors path) solns))))))
+
+(defn- find-passcodes [graph]
+  (let [universe   (keys graph)
+        target-len (count universe)]
+    (flatten (map #(find-solution % graph universe target-len) universe))))
+
+(defn passcode-derivation [& [file]]
+  (->> (or file "data/079.txt")
+       (slurp)
+       (re-seq #"\d\d\d")
+       (distinct)
+       (graph-of-tries)
+       (find-passcodes)))
