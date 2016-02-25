b2fb5ecb8bdefb40b7cae1227940fbe135a936c9
Randy J. Ray <rjray@blackperl.com>
2016-02-25 11:41:22 -0800

    Move clojure version to 1.8.0.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.combinatorics "0.1.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]])
