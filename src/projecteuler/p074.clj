e5011f16fa5b80677d22157de6fff7ac7a9f3cf8
Randy J. Ray <rjray@blackperl.com>
2016-03-04 11:15:00 -0800

    Solution for #74.

---
+++ src/projecteuler/p074.clj
@@ -0,0 +1,20 @@
+(ns projecteuler.p074)
+
+;; https://projecteuler.net/problem=74
+
+(defn- digit-fact-sum [n]
+  (reduce + (map ! (map num-map (str n)))))
+
+(defn- find-chain-length [n]
+  (loop [num n, chain #{}]
+    (cond
+     (chain num) (list n (count chain))
+     :else       (recur (digit-fact-sum num) (conj chain num)))))
+
+(defn digit-factorial-chains [& [maxn length]]
+  (let [maxn   (or maxn 1000000)
+        length (or length 60)]
+    (->> (range 1 maxn)
+         (map find-chain-length)
+         (filter #(= (second %) length))
+         (count))))
