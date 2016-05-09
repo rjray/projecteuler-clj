01aab5b4eae24ff91f15cb6be5209d91ab05b2e0
Randy J. Ray <rjray@blackperl.com>
2016-05-09 14:16:25 -0700

    Solution for #87.

---
+++ src/projecteuler/p087.clj
@@ -0,0 +1,33 @@
+(ns projecteuler.p051
+  (:require [projecteuler.core :refer [primes]]))
+
+;; https://projecteuler.net/problem=87
+
+;; Brute-force approach adapted from
+;; http://www.mathblog.dk/project-euler-87-sum-power/
+
+(defn prime-power-triples [& [maxn]]
+  (let [maxn      (or maxn 50000000)
+        maxp      (int (Math/sqrt maxn))
+        primelist (take-while #(< % maxp) primes)
+        pl2       (map #(* % %) primelist)
+        pl3       (map #(* % % %) primelist)
+        pl4       (map #(* % %) pl2)]
+    (loop [[square & list1] pl2, nums #{}]
+      (cond
+       (nil? square) (count nums)
+       :else
+       (recur list1
+              (loop [[cube & list2] pl3, nums nums]
+                (cond
+                 (nil? cube)              nums
+                 (> (+ square cube) maxn) nums
+                 :else
+                 (recur list2
+                        (loop [[quad & list3] pl4, nums nums]
+                          (let [sum (+ square cube quad)]
+                              (cond
+                               (nil? quad)  nums
+                               (> sum maxn) nums
+                               :else
+                               (recur list3 (conj nums sum)))))))))))))
