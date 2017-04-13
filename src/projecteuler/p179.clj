4eab49ffb27cfe444fa77a41a5b9000af0d2480d
Randy J. Ray <rjray@blackperl.com>
2017-04-13 16:13:37 -0700

    Solution for #179.

---
+++ src/projecteuler/p179.clj
@@ -0,0 +1,29 @@
+(ns projecteuler.p179)
+
+;; https://projecteuler.net/problem=179
+
+;; Based on the dynamic-ish algorithm at
+;; https://blog.dreamshire.com/project-euler-179-solution/
+
+(defn- apply-factor [i n v]
+  (loop [j (* i 2), v v]
+    (cond
+     (>= j n) v
+     :else   (recur (+ j i) (update v j inc)))))
+
+;; Slow. Try to parallel-ize this function.
+(defn- dynamic-divisors [n]
+  (let [v (vec (repeat n 0))
+        m (int (/ n 2))]
+    (loop [i 2, v v]
+      (cond
+       (> i m) v
+       :else   (recur (inc i) (apply-factor i n v))))))
+
+(defn consecutive-positive-divisors [& [maxn]]
+  (let [n  (or maxn 10000000)
+        dv (dynamic-divisors (vec (repeat n 0)))]
+    (apply + (for [i (range 3 n)]
+               (cond
+                (= (dv i) (dv (dec i))) 1
+                :else                   0)))))
