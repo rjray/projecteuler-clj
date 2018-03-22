f40abc264ffd5ffacdc527f13d9b1914a5a2228a
Randy J. Ray <rjray@blackperl.com>
2018-03-22 11:01:33 -0700

    Added pprint and kibit plugins to lein.

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.combinatorics "0.1.1"]
-                 [org.clojure/math.numeric-tower "0.0.4"]])
+                 [org.clojure/math.numeric-tower "0.0.4"]]
+  :plugins [[lein-kibit "0.1.6"]
+            [lein-pprint "1.2.0"]])
